f437e9d5bbb8c680d0667e934dc04376c6319d84
Ashin007 <ashinvijayan007@gmail.com>
2021-06-29 21:37:49 +0530

    dynamic table compatibility added

--- src/test/java/testcases/PopulationTest.java
+++ src/test/java/testcases/PopulationTest.java
@@ -34,6 +34,8 @@ public class PopulationTest extends Base {
 //            System.out.println(webElement.getText());
 //        }
 
+        int table_row = driver.findElements(By.xpath("//*[@id='liveWorldPop']/div[1]/div/div/div/div/div[2]/div[2]/div[2]/table/tbody/tr")).size();
+        int table_col = driver.findElements(By.xpath("//*[@id='liveWorldPop']/div[1]/div/div/div/div/div[2]/div[2]/div[2]/table/thead/tr/th")).size();
 
         String heading_xpath_first = "//*[@id='liveWorldPop']/div[1]/div/div/div/div/div[2]/div[2]/div[2]/table/thead/tr/th[";
         String heading_xpath_last = "]/span/a";
@@ -43,9 +45,9 @@ public class PopulationTest extends Base {
         String table_data_last = "]";
         int k=0;
 
-        for(int i=1;i<=232;i++){
+        for(int i=1;i<=table_row;i++){
 
-            for(int j=1;j<10;j++){
+            for(int j=1;j<table_col;j++){
                 System.out.print(driver.findElement(By.xpath(heading_xpath_first+j+heading_xpath_last)).getText()+"  : ");
                 System.out.println(driver.findElement(By.xpath(table_row_first+i+table_row_last+j+table_data_last)).getText());
 
