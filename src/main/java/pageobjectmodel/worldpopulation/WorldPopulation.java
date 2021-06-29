6e2abfd0af93c99d06cd3af2cf4efb01f544b24f
Ashin007 <ashinvijayan007@gmail.com>
2021-06-29 22:39:07 +0530

    page object model implemented -population

--- src/main/java/pageobjectmodel/worldpopulation/WorldPopulation.java
+++ src/main/java/pageobjectmodel/worldpopulation/WorldPopulation.java
@@ -1,13 +1,52 @@
 package pageobjectmodel.worldpopulation;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class WorldPopulation {
+
+    //web driver reference
     private WebDriver driver;
 
-    //private By
+    //raw xpath of row and column
+    private String table_row_xpath = "//*[@id='liveWorldPop']/div[1]/div/div/div/div/div[2]/div[2]/div[2]/table/tbody/tr";
+    private String table_col_xpath = "//*[@id='liveWorldPop']/div[1]/div/div/div/div/div[2]/div[2]/div[2]/table/thead/tr/th";
+
+
+    private By table_row = By.xpath(table_row_xpath);
+    private By table_col = By.xpath(table_col_xpath);
+
+    //splitting col xpath
+    private String heading_xpath_first =table_col_xpath+"[";
+    private String heading_xpath_last = "]/span/a";
+
+    //splitting row xpath
+    private String table_row_first = table_row_xpath+"[";
+    private String table_row_last = "]/td[";
+    private String table_data_last = "]";
 
     public WorldPopulation(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public int getTable_row_size() {
+        return driver.findElements(table_row).size();
+
+    }
+
+    public int getTable_col_size() {
+        return driver.findElements(table_col).size();
+    }
+
+    public void getTableData(){
+        for(int i=1;i<=getTable_row_size();i++){
+            if(i==3)break;
+
+            for(int j=1;j<getTable_col_size();j++){
+                System.out.print(driver.findElement(By.xpath(heading_xpath_first+j+heading_xpath_last)).getText()+"  : ");
+                System.out.println(driver.findElement(By.xpath(table_row_first+i+table_row_last+j+table_data_last)).getText());
+
+            }
+        }
     }
 }
--- src/test/java/testcases/PopulationTest.java
+++ src/test/java/testcases/PopulationTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
+import pageobjectmodel.worldpopulation.WorldPopulation;
 
 import java.io.IOException;
 import java.util.List;
@@ -26,35 +27,11 @@ public class PopulationTest extends Base {
     @Test
     public void mainTest(){
 
+        //target url
         driver.get("https://worldpopulationreview.com/");
-//        WebElement element = driver.findElements(By.xpath("//table[@class='jsx-1487038798 table table-striped tp-table-body']")).get(0);
-//        List<WebElement> dataList = element.findElements(By.tagName("tr"));
-//
-//        for (WebElement webElement:dataList){
-//            System.out.println(webElement.getText());
-//        }
 
-        int table_row = driver.findElements(By.xpath("//*[@id='liveWorldPop']/div[1]/div/div/div/div/div[2]/div[2]/div[2]/table/tbody/tr")).size();
-        int table_col = driver.findElements(By.xpath("//*[@id='liveWorldPop']/div[1]/div/div/div/div/div[2]/div[2]/div[2]/table/thead/tr/th")).size();
-
-        String heading_xpath_first = "//*[@id='liveWorldPop']/div[1]/div/div/div/div/div[2]/div[2]/div[2]/table/thead/tr/th[";
-        String heading_xpath_last = "]/span/a";
-
-        String table_row_first = "//*[@id='liveWorldPop']/div[1]/div/div/div/div/div[2]/div[2]/div[2]/table/tbody/tr[";
-        String table_row_last = "]/td[";
-        String table_data_last = "]";
-        int k=0;
-
-        for(int i=1;i<=table_row;i++){
-
-            for(int j=1;j<table_col;j++){
-                System.out.print(driver.findElement(By.xpath(heading_xpath_first+j+heading_xpath_last)).getText()+"  : ");
-                System.out.println(driver.findElement(By.xpath(table_row_first+i+table_row_last+j+table_data_last)).getText());
-
-            }
-        }
-
-
+        WorldPopulation population = new WorldPopulation(driver);
+        population.getTableData();
 
     }
 
